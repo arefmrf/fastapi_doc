a9103a11f47395cc53e94511e58f3967b7b2027b
aref <you@example.com>
2025-01-24 22:41:43 +0330

    asd

--- GO/concurrency/main.go
+++ GO/concurrency/main.go
@@ -11,13 +11,12 @@ func printIt(s string, wg *sync.WaitGroup) {
 }
 
 func main() {
-	words := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
+	/* words := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
 	//wg := sync.WaitGroup  why its not working? why var x struct works?
 	var wg sync.WaitGroup
 	wg.Add(len(words))
 	for i, word := range words {
 		go printIt(fmt.Sprintf("%d: %s", i, word), &wg)
 	}
-	wg.Wait()
-
+	wg.Wait() */
 }
