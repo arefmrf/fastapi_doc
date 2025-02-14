640e621e73e0e20bb006b11f8c12321a526a30d6
aref <you@example.com>
2025-02-14 23:00:07 +0330

    ssss

--- GO/sync/9.go
+++ GO/sync/9.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.WithValue(context.Background(), "language", "Go")
+
+	fmt.Println(manager(ctx, "language"))
+}
+
+func manager(ctx context.Context, key string) string {
+	if v := ctx.Value(key); v != nil {
+
+		fmt.Printf("%T\n", v)
+		return v.(string)
+	}
+	return "not found value"
+}
